8dcc7f5a59682c89b9b74a13e60a055b44d5150b
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-10 17:48:47 +1000

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -3,3 +3,8 @@ window.pricePerHand = 10;
 window.money = 0;
 window.psi = 0;
 window.hands = 0;
+window.psipersec = 0;
+
+//Element Variables
+window.psiValue = document.getElementById("psiValue");
+window.psiPerSec = document.getElementById("psiPerSec");
