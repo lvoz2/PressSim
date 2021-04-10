cc720d4f8abb105df5655b1207ada283ff4e538c
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-10 15:48:15 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,8 @@
-
+buyHands(handsToBuy) {
+  if ((handsToBuy * pricePerHand) >= money) {
+    hands = hands + handsToBuy;
+  }
+	if ((handsToBuy * pricePerHand) < money) {
+		alert("You do not have enough money to buy "handsToBuy" hands");
+	}
+}
