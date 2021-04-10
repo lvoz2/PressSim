5bf72102a65f407c1e4cc2a499f10fc3335b04f8
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-10 17:01:04 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -6,10 +6,10 @@ function gainPsi(psiToIncreaseBy) {
 	refresh()
 }
 function buyHands(handsToBuy) {
-	if ((handsToBuy * pricePerHand) >= money) {
+	if ((handsToBuy * pricePerHand) <= money) {
 		hands = hands + handsToBuy;
 	}
-	if ((handsToBuy * pricePerHand) < money) {
+	if ((handsToBuy * pricePerHand) > money) {
 		alert("You do not have enough money to buy that many hands");
 	}
 	refresh()
