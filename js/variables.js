b150e25a35473684e1104ee0b42f9eab19c89fa5
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-10 16:46:26 +1000

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,4 @@
-function initiateVariables() {
-	//Javascript Internal Variables
-	window.pricePerHand = 10;
-	window.money = 0;
-	window.psi = 0;
-}
+//Javascript Internal Variables
+window.pricePerHand = 10;
+window.money = 0;
+window.psi = 0;
