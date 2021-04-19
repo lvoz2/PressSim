f54aa83ad3ca52169fc241bcaf9c278d03d8d6f6
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-19 11:52:42 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,53 @@
 function load() {
 	location.replace("https://lvoz2.github.io/PressSim/#pressure_creation")
+	loadSave()
+}
+function loadSave() {
+	var save = localStorage.getItem('save');
+	save = parseInt(save);
+	if (save == 2) {
+		var pricePerHandfile = localStorage.getItem('pricePerHand');
+		var moneyfile = localStorage.getItem('money'); 
+		var psifile = localStorage.getItem('psi'); 
+		var handsfile = localStorage.getItem('hands'); 
+		var psipersecfile = localStorage.getItem('psipersec');
+		var handMultiplierfile = localStorage.getItem('handMultipler'); 
+		var researchpointcostfile = localStorage.getItem('researchpointcost'); 
+		var researchpointsfile = localStorage.getItem('researchpoints'); 
+		var moneycostfile = localStorage.getItem('moneycost'); 
+		var chemistrykitfile = localStorage.getItem('chemistrykit'); 
+		var chemistrykitMultiplierfile = localStorage.getItem('chemistrykitMultiplier'); 
+		var salespersonfile = localStorage.getItem('salesperson'); 
+		var salespersonMultiplierfile = localStorage.getItem('salespersonMultiplier');
+		pricePerHandfile = atob(pricePerHandfile); 
+		moneyfile = atob(moneyfile); 
+		psifile = atob(psifile); 
+		handsfile = atob(handsfile); 
+		psipersecfile = atob(psipersecfile); 
+		handMultiplierfile = atob(handMultiplierfile); 
+		researchpointcostfile = atob(researchpointcostfile); 
+		researchpointsfile = atob(researchpointsfile); 
+		moneycostfile = atob(moneycostfile); 
+		chemistrykitfile = atob(chemistrykitfile); 
+		chemistrykitMultiplierfile = atob(chemistrykitMultiplierfile); 
+		salespersonfile = atob(salespersonfile); 
+		salespersonMultiplierfile = atob(salespersonMultiplierfile);
+		pricePerHand = parseInt(pricePerHandfile); 
+		money = parseInt(moneyfile); 
+		psi = parseInt(psifile); 
+		hands = parseInt(handsfile); 
+		psipersec = parseInt(psipersecfile); 
+		handMultiplier = parseInt(handMultiplierfile); 
+		researchpointcost = parseInt(researchpointcostfile); 
+		researchpoints = parseInt(researchpointsfile); 
+		moneycost = parseInt(moneycostfile); 
+		chemistrykit = parseInt(chemistrykitfile); 
+		chemistrykitMultiplier = parseInt(chemistrykitMultiplierfile); 
+		salesperson = parseInt(salespersonfile); 
+		salespersonMultiplier = parseInt(salespersonMultiplierfile);
+	}
+	if (save!= 2) {
+	}
 	refresh()
 }
 function gainPsi(psiToIncreaseBy) {
@@ -55,5 +103,25 @@ function functionAlert(msg, myYes) {
 	});
 	confirmBox.find(".yes").click(myYes);
 	confirmBox.show();
+}
+function savetoLS(key, value) {
+	var lssave = btoa(value);
+	localStorage.setItem(key, lssave);
+}
+function updateSaveFile() {
+	savetoLS('pricePerHand', pricePerHand);
+	savetoLS('money', money);
+	savetoLS('psi', psi);
+	savetoLS('hands', hands);
+	savetoLS('psipersec', psipersec);
+	savetoLS('handMultiplier', handMultiplier);
+	savetoLS('researchpointcost', researchpointcost);
+	savetoLS('researchpoints', researchpoints);
+	savetoLS('moneycost', moneycost);
+	savetoLS('chemistrykit', chemistrykit);
+	savetoLS('chemistrykitMultiplier', chemistrykitMultiplier);
+	savetoLS('salesperson', salesperson);
+	savetoLS('salespersonMultiplier', salespersonMultiplier);
+	savetoLS('save', 2);
 }
 load()
