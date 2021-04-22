0e45906a5d4fda60709b381011ea0971c1f47faa
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-04-22 13:08:30 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -106,15 +106,6 @@ function refresh() {
 function update() {
 	gainPsi(psipersec)
 }
-function functionAlert(msg, myYes) {
-	var confirmBox = $("#confirm");
-	confirmBox.find(".message").text(msg);
-	confirmBox.find(".yes").unbind().click(function() {
-		confirmBox.hide();
-	});
-	confirmBox.find(".yes").click(myYes);
-	confirmBox.show();
-}
 function savetoLS(key, value) {
 	var lssave = btoa(value);
 	localStorage.setItem(key, lssave);
