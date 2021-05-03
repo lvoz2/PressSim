41b028bb62d0e9abf3b3d8db6fd79dbb6f20e76b
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 15:25:51 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,17 +1,6 @@
 function load() {
 	location.replace("https://lvoz2.github.io/PressSim/#pressure_creation")
 	variables()
-	window.worker = new Worker("./worker.js");
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker
-		.register("https://lvoz2.github.io/PressSim/worker.js")
-		.then(serviceWorker => {
-			console.log("Service Worker registered: ", serviceWorker);
-		})
-		.catch(error => {
-			console.error("Error registering the Service Worker: ", error);
-		});
-	}
 	setInterval(update, 10);
 	checkAlertify()
 	loadSave()
