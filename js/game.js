7e598cf41e4fa6322b33c37238fd731b41060468
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 12:20:01 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 function load() {
 	location.replace("https://lvoz2.github.io/PressSim/#pressure_creation")
 	variables()
+	window.worker = new Worker("worker.js");
 	setInterval(update, 10);
 	checkAlertify()
 	loadSave()
