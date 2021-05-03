735a0e4c9c2b1828ad371d10cd06490353371449
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 10:48:34 +1000

    Create startup.js

---
+++ js/startup.js
@@ -0,0 +1,6 @@
+var script1 = document.createElement('script')
+script.src = './js/game.js'
+document.head.append(script)
+var script2 = document.createElement('script')
+script.src = './js/variables.js'
+document.head.append(script)
