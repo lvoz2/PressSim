cda28bb1454ca908952eaa3edb0e73f718b7723d
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 15:38:46 +1000

    Create variables.js

---
+++ js/variables.js
@@ -0,0 +1,36 @@
+function variables() {
+	//Javascript Internal Variables
+	window.pricePerHand = 4;
+	window.money = 0;
+	window.psi = 0;
+	window.hands = 0;
+	window.psipersec = 0;
+	window.researchpersec = 0;
+	window.handMultiplier = 1;
+	window.researchpointcost = 10;
+	window.researchpoints = 0;
+	window.moneycost = 1;
+	window.kits = 0; 
+	window.Akits = 0;
+	window.Managers = 0;
+	window.pricePerKit = 5;
+	window.pricePerAKit = 50;
+	window.pricePerManager = 20;
+	window.pricePerSalesperson = 5;
+	window.pricePerSalespersonrp = 1;
+	window.pricePerKitSpeedCost = 10;
+	window.kitMultiplier = 1;
+	window.AkitMultiplier = 10;
+	window.ManagerMultiplier = 10;
+	window.salesperson = 0;
+	window.salespersonMultiplier = 1;
+	window.save = 0;
+
+	//Element Variables
+	window.psiValue = document.getElementById("psiValue");
+	window.psiPerSec = document.getElementById("psiPerSec");
+	window.researchValue = document.getElementById("researchValue");
+	window.researchPerSec = document.getElementById("researchPerSec");
+	window.coinsValue = document.getElementById("coinsValue"); 
+	window.coinsPerSec = document.getElementById("coinsPerSec");
+}
