8376bd0ea23f84b51b69eef7979560797d0cd45d
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 15:40:24 +1000

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,11 +1,18 @@
-window.worker = new Worker("./worker.js");
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker
-		.register("https://lvoz2.github.io/PressSim/worker.js")
-		.then(serviceWorker => {
-			console.log("Service Worker registered: ", serviceWorker);
-		})
-		.catch(error => {
-			console.error("Error registering the Service Worker: ", error);
-		});
-	}
+function createscript1() {
+	var script1 = document.createElement('script')
+	script1.src = './alertifyjs/alertify.min.js'
+	document.head.append(script1)
+	setTimeout(createscript2, 100)
+}
+function createscript2() {
+	var script2 = document.createElement('script')
+	script2.src = './js/variables.js'
+	document.head.append(script2)
+	setTimeout(createscript3, 100)
+}
+function createscript3() {
+	var script3 = document.createElement('script')
+	script3.src = './js/game.js'
+	document.head.append(script3)
+}
+createscript1()
