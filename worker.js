7b2cd94a4e693f6b0efe54a7db7b4e9a8f530276
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 15:26:35 +1000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,22 @@
+function createscript1() {
+	var script1 = document.createElement('script')
+	script1.src = './alertifyjs/alertify.min.js'
+	document.head.append(script1)
+	setTimeout(createscript2, 100)
+}
+function createscript2() {
+	var script2 = document.createElement('script')
+	script2.src = './js/variables.js'
+	document.head.append(script2)
+	setTimeout(createscript3, 100)
+}
+function createscript3() {
+	var script3 = document.createElement('script')
+	script3.src = './js/game.js'
+	document.head.append(script3)
+}
+createscript1()
+
 var CACHE_NAME = 'PressSim';
 var urlsToCache = [
   'https://lvoz2.github.io/PressSim/index.html'
