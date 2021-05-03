012ed0bdbb0ed93a06f4b308201db63a6c70ffae
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-03 10:51:25 +1000

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,9 +1,9 @@
 var script1 = document.createElement('script')
-script.src = './alertifyjs/alertify.min.js'
-document.head.append(script)
+script1.src = './alertifyjs/alertify.min.js'
+document.head.append(script1)
 var script2 = document.createElement('script')
-script.src = './js/game.js'
-document.head.append(script)
+script2.src = './js/game.js'
+document.head.append(script2)
 var script3 = document.createElement('script')
-script.src = './js/variables.js'
-document.head.append(script)
+script3.src = './js/variables.js'
+document.head.append(script3)
