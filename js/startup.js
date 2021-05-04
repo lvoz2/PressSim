632d5472c4386ed9e8872f9599c55821da73ba6b
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-04 11:09:17 +1000

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,19 +1,19 @@
 function createscript1() {
 	var script1 = document.createElement('script')
 	script1.src = './alertifyjs/alertify.min.js'
-	script1.onload = createscript2()
+	script1.onload = function(){createscript2()}
 	document.head.append(script1)
 }
 function createscript2() {
 	var script2 = document.createElement('script')
 	script2.src = './js/variables.js'
-	script2.onload = createscript3()
+	script2.onload = function(){createscript3()}
 	document.head.append(script2)
 }
 function createscript3() {
 	var script3 = document.createElement('script')
 	script3.src = './js/game.js'
-	script3.onload = createscript4()
+	script3.onload = function(){createscript4()}
 	document.head.append(script3)
 }
 function createscript4() {
