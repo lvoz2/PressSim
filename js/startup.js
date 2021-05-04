e21b4833a70eb443e07b8adb4c6f789958053410
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-04 10:58:06 +1000

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,20 +1,20 @@
 function createscript1() {
 	var script1 = document.createElement('script')
 	script1.src = './alertifyjs/alertify.min.js'
-	document.head.append(script1)
 	script1.onload = createscript2()
+	document.head.append(script1)
 }
 function createscript2() {
 	var script2 = document.createElement('script')
 	script2.src = './js/variables.js'
-	document.head.append(script2)
 	script2.onload = createscript3()
+	document.head.append(script2)
 }
 function createscript3() {
 	var script3 = document.createElement('script')
 	script3.src = './js/game.js'
-	document.head.append(script3)
 	script3.onload = createscript4()
+	document.head.append(script3)
 }
 function createscript4() {
 	var script4 = document.createElement('script')
