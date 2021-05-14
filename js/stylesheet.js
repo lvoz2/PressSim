f24f7a3be7f811bb763b21754f6c750187711ab9
K9R <56783073+RealK9R@users.noreply.github.com>
2021-05-14 21:18:44 +1000

    test

---
+++ js/stylesheet.js
@@ -0,0 +1,11 @@
+function swapStyleSheetscheme(schemejs1){
+	document.getElementById('schemecss').setAttribute('href', schemejs1);
+}
+
+function swapStyleSheetlightdark(lightdarkjs1){
+	document.getElementById('lightdarkcss').setAttribute('href', lightdarkjs1);
+}
+
+function swapStyleSheetfont(fontjs1){
+	document.getElementById('fontcss').setAttribute('href', fontjs1);
+}
