4584be9217f93d830ae6be6caa69777a85b78419
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-17 13:36:31 +1000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -129,19 +129,19 @@ function changeScheme() {
 function changeLightdark() {
 	window.currentscheme = document.getElementById("lightdark").options[document.getElementById("lightdark").selectedIndex].value;
 	if (currentscheme === "light") {
-		swapStyleSheetslightdark('./css/lightdark/light.css')
+		swapStyleSheetlightdark('./css/lightdark/light.css')
 	}
 	if (currentscheme === "dark") {
-		swapStyleSheetslightdark('./css/lightdark/dark.css')
+		swapStyleSheetlightdark('./css/lightdark/dark.css')
 	}
 }
 function changeFont() {
 	window.currentscheme = document.getElementById("font").options[document.getElementById("font").selectedIndex].value;
 	if (currentscheme === "quicksand") {
-		swapStyleSheetslightdark('./css/fonts/quicksand.css')
+		swapStyleSheetlightdark('./css/fonts/quicksand.css')
 	}
 	if (currentscheme === "arvo") {
-		swapStyleSheetslightdark('./css/fonts/arvo.css')
+		swapStyleSheetlightdark('./css/fonts/arvo.css')
 	}
 }
 function stopLoad() {
